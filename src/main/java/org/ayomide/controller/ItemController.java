946002ba6806624d40baaef4f124e330f3020dff
akinolablessing <akinolablessing1890@gmail.com>
2025-04-22 08:06:50 +0100

    item controller done successful

--- src/main/java/org/ayomide/controller/ItemController.java
+++ src/main/java/org/ayomide/controller/ItemController.java
@@ -1,4 +1,34 @@
 package org.ayomide.controller;
 
+import org.ayomide.controller.dto.request.ItemDtoRequest;
+import org.ayomide.controller.dto.request.RegisterUserRequest;
+import org.ayomide.controller.dto.response.ApiResponse;
+import org.ayomide.controller.dto.response.ItemDtoResponse;
+import org.ayomide.controller.dto.response.RegisterUserResponse;
+import org.ayomide.services.ItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@AllArgsConstructor
+@RequestMapping("/api/item")
 public class ItemController {
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+    @PostMapping("/add-items")
+    public ResponseEntity<?> addItem(@RequestBody ItemDtoRequest itemDtoRequest){
+        try {
+            ItemDtoResponse response =  itemService.createItem(itemDtoRequest);
+            return new ResponseEntity<>(new ApiResponse(true, response), HttpStatus.OK);
+        }catch(Exception exception){
+            return new ResponseEntity<>(new ApiResponse(false, exception.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/test/java/ItemServiceTest.java
+++ src/test/java/ItemServiceTest.java
@@ -1,2 +1,34 @@
+import org.ayomide.Main;
+import org.ayomide.controller.dto.request.ItemDtoRequest;
+import org.ayomide.controller.dto.response.ItemDtoResponse;
+import org.ayomide.services.ItemService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = Main.class)
 public class ItemServiceTest {
+    private final ItemService itemService;
+
+    @Autowired
+    public ItemServiceTest(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @Test
+    public void testThatCreateItem() {
+        ItemDtoRequest itemDtoRequest = addItem();
+        ItemDtoResponse itemDtoResponse = itemService.createItem(itemDtoRequest);
+        assertEquals("Item Created Successful", itemDtoResponse.getMessage());
+    }
+    public ItemDtoRequest addItem(){
+        ItemDtoRequest request = new ItemDtoRequest();
+        request.setName("Bag");
+        request.setDescription("I want black and portable size");
+        request.setAmount(5000.00);
+        return  request;
+    }
+
 }
